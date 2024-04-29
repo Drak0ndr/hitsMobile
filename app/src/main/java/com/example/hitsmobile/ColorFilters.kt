5764ab7319b89f04f161b94ade835a9b3ea81571
Максим <drakondr.video@gmail.com>
2024-04-29 08:24:44 +0700

    add 3 color filters

--- app/src/main/java/com/example/hitsmobile/ColorFilters.kt
+++ app/src/main/java/com/example/hitsmobile/ColorFilters.kt
@@ -5,23 +5,19 @@ import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import java.io.ByteArrayOutputStream
 import android.graphics.Bitmap
 import android.graphics.Color
-import androidx.core.graphics.set
 
-class Rotate: AppCompatActivity() {
+class ColorFilters: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.test_rotate)
+        setContentView(R.layout.test_color_filters)
         var img = findViewById<ImageView>(R.id.imageView)
         var bitmap = (img.getDrawable() as BitmapDrawable).bitmap
         var test = bitmap.getPixel(0,0)
-        var ans = rotateLeft(bitmap)
-//        img.setImageBitmap(ans)
+        var ans = toGray(bitmap)
+        img.setImageBitmap(ans)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,48 +25,74 @@ class Rotate: AppCompatActivity() {
 //        }
 
     }
-    fun rotateRight(bitmap: Bitmap): Bitmap {
-        var rotatedBitmap = Bitmap.createBitmap(bitmap.height, bitmap.width,Bitmap.Config.ARGB_8888)
+    fun toGreen(bitmap: Bitmap): Bitmap {
         var width = bitmap.width
         var height = bitmap.height
+        var newBitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888)
         var x = 0
         var y = 0
         while (x < width) {
             y = 0
-            while (y< height) {
+            while (y < height) {
                 var colorPixel = bitmap.getColor(x,y).components
                 var red = colorPixel[0]
                 var green = colorPixel[1]
                 var blue = colorPixel[2]
                 var alfa = colorPixel[3]
-                rotatedBitmap.setPixel(height -1 -y, x, Color.argb(alfa, red, green, blue))
-                y++
-            }
-            x++
-        }
-        return rotatedBitmap
-    }
-    fun rotateLeft(bitmap: Bitmap): Bitmap {
-        var rotatedBitmap = Bitmap.createBitmap(bitmap.height, bitmap.width,Bitmap.Config.ARGB_8888)
-        var width = bitmap.width
-        var height = bitmap.height
-        var x = 0
-        var y = 0
-        while (x < width) {
-            y = 0
-            while (y< height) {
-                var colorPixel = bitmap.getColor(x,y).components
-                var red = colorPixel[0]
-                var green = colorPixel[1]
-                var blue = colorPixel[2]
-                var alfa = colorPixel[3]
-                rotatedBitmap.setPixel(y, width -1 - x, Color.argb(alfa, red, green, blue))
-                y++
-            }
-            x++
-        }
-        return rotatedBitmap
-    }
 
+                newBitmap.setPixel(x, y, Color.argb(alfa, 0f, green, 0f))
+                y++
+            }
+            x++
+        }
+
+        return newBitmap
+    }
+    fun toNegative(bitmap: Bitmap): Bitmap {
+        var width = bitmap.width
+        var height = bitmap.height
+        var newBitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888)
+        var x = 0
+        var y = 0
+        while (x < width) {
+            y = 0
+            while (y < height) {
+                var colorPixel = bitmap.getColor(x,y).components
+                var red = colorPixel[0]
+                var green = colorPixel[1]
+                var blue = colorPixel[2]
+                var alfa = colorPixel[3]
+
+                newBitmap.setPixel(x, y, Color.argb(alfa, 1 - red,1- green, 1 - blue))
+                y++
+            }
+            x++
+        }
+
+        return newBitmap
+    }
+    fun toGray(bitmap: Bitmap): Bitmap {
+        var width = bitmap.width
+        var height = bitmap.height
+        var newBitmap = Bitmap.createBitmap(width, height,Bitmap.Config.ARGB_8888)
+        var x = 0
+        var y = 0
+        while (x < width) {
+            y = 0
+            while (y < height) {
+                var colorPixel = bitmap.getColor(x,y).components
+                var red = colorPixel[0]
+                var green = colorPixel[1]
+                var blue = colorPixel[2]
+                var alfa = colorPixel[3]
+                var gray = 0.2126f * red + 0.7152f * green + 0.0722f * blue
+                newBitmap.setPixel(x, y, Color.argb(alfa, gray,gray, gray))
+                y++
+            }
+            x++
+        }
+
+        return newBitmap
+    }
 
 }
