7ee0af997c6745e5911d64e9fedc6b6a9d22c394
Максим <drakondr.video@gmail.com>
2024-05-07 18:29:08 +0700

    add gaus blur

--- app/src/main/java/com/example/hitsmobile/filters/ColorFilters.kt
+++ app/src/main/java/com/example/hitsmobile/filters/ColorFilters.kt
@@ -138,4 +138,63 @@ class ColorFilters: PhotoActivity() {
         }
         return newBitmap
     }
+    fun gaussian(sigma: Float, y:Int,x:Int): Double {
+        var temp =  (- (x*x + y*y) / (2 * sigma.pow(2))).toDouble()
+        var ans = (1 / (3.14 * sigma.pow(2)) * 2.71.pow(temp))
+        return ans
+    }
+    fun gausBlur(bitmap: Bitmap, r:Float): Bitmap {
+        var width = bitmap.width
+        var height = bitmap.height
+        var newBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        var kernel = mutableListOf<MutableList<Double>>()
+        var len = (r).toInt() * 2 + 1
+        var sigma = r/3
+        var mid = (r).toInt()
+        var i = 0
+        while (i < len) {
+            var temp = mutableListOf<Double>()
+            var j = 0
+            while (j < len) {
+                temp.add(gaussian(sigma, i-mid, j-mid))
+                j++
+            }
+            kernel.add(temp)
+            i++
+        }
+
+        i = mid
+        while (i < bitmap.height - mid) {
+            var j = mid
+            while (j < bitmap.width - mid) {
+                var tempRed = 0f
+                var tempGreen = 0f
+                var tempBlue = 0f
+                var tempAlpha = 0f
+                var kernelCoef = 0.0
+                var y = 0
+                while (y < len) {
+                    var x = 0
+                    while (x < len) {
+                        var tempColors = bitmap.getColor(j - mid + x, i - mid + y).components
+                        tempRed+= (tempColors[0] * kernel[y][x]).toFloat()
+                        tempGreen+= (tempColors[1] * kernel[y][x]).toFloat()
+                        tempBlue+= (tempColors[2] * kernel[y][x]).toFloat()
+                        tempAlpha+= (tempColors[3] * kernel[y][x]).toFloat()
+                        kernelCoef+= kernel[y][x]
+                        x++
+                    }
+                    tempRed = (tempRed / kernelCoef).toFloat()
+                    tempGreen = (tempGreen / kernelCoef).toFloat()
+                    tempBlue = (tempBlue / kernelCoef).toFloat()
+                    tempAlpha = (tempAlpha / kernelCoef).toFloat()
+                    newBitmap.setPixel(j,i, Color.argb(tempAlpha, tempRed, tempGreen, tempBlue))
+                    y++
+                }
+                j++
+            }
+            i++
+        }
+        return newBitmap
+    }
 }
