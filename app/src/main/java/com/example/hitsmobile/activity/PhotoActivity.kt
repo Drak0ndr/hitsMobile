6b57b32c17549825c2e4db60330774ce2d5db57d
Anastasia <158266030+AnastasiaStavrova@users.noreply.github.com>
2024-05-11 21:00:08 +0700

    fix

--- app/src/main/java/com/example/hitsmobile/activity/PhotoActivity.kt
+++ app/src/main/java/com/example/hitsmobile/activity/PhotoActivity.kt
@@ -158,7 +158,7 @@ open class PhotoActivity: AppCompatActivity(), OnItemSelected, FilterViewAdapter
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 val selectedItem = parent.getItemAtPosition(position).toString()
-                Toast.makeText(applicationContext, "Фильтрация: $selectedItem", Toast.LENGTH_SHORT).show()
+
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
