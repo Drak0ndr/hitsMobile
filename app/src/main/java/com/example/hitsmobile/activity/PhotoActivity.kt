23aef2d77c3231494a4df552863f863633394ab8
Anastasia <158266030+AnastasiaStavrova@users.noreply.github.com>
2024-05-24 19:12:49 +0700

    connecting the transformation

--- app/src/main/java/com/example/hitsmobile/activity/PhotoActivity.kt
+++ app/src/main/java/com/example/hitsmobile/activity/PhotoActivity.kt
@@ -45,6 +45,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
 import com.example.hitsmobile.R
+import com.example.hitsmobile.algorithms.Affinis
 import com.example.hitsmobile.algorithms.Mask
 import com.example.hitsmobile.algorithms.MyDialogFragment
 import com.example.hitsmobile.algorithms.Resize
@@ -266,6 +267,14 @@ open class PhotoActivity: AppCompatActivity(), OnItemSelected, FilterViewAdapter
                 blueImg.visibility = View.GONE
                 greenImg.visibility = View.GONE
 
+                val affinis = Affinis()
+
+                val transformImg = affinis.transformBitmap(MyVariables.rotateImg, listSecondPoints[0].first, listSecondPoints[0].second,
+                    listSecondPoints[1].first, listSecondPoints[1].second, listSecondPoints[2].first, listSecondPoints[2].second,
+                    listFirstPoints[0].first, listFirstPoints[0].second, listFirstPoints[1].first, listFirstPoints[1].second,
+                    listFirstPoints[2].first, listFirstPoints[2].second)
+
+                imageView.setImageBitmap(transformImg)
 
                 listSecondPoints.clear()
                 listFirstPoints.clear()
